edfef168b362e91dc7919980a61908c14f32fcb6
Rawan <rawanelashmawyy@gmail.com>
2023-05-30 00:10:40 +0300

    FINALLLLLL

---
+++ OnlineBankingSystem/logout.js
@@ -0,0 +1,7 @@
+
+function logout() {
+  // Perform logout actions here
+  alert("Logged out successfully!");
+  // Redirect to login page or perform any other required action
+  href = "Login.html";
+}
